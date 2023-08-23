6bba41d9ed6d3f3fae935ee479ffff7aedac3f93
amirn003 <amir.naar@gmail.com>
2023-08-23 11:39:11 +0200

    fix create issue, add edit/update actions

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,5 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:edit, :update]
 
   def index
     @flats = Flat.all
@@ -10,22 +11,38 @@ class FlatsController < ApplicationController
 
   def create
     @flat = Flat.new(flat_params)
+    @flat.user = current_user
 
     if @flat.save
-      redirect_to flat_path(flat)
+      redirect_to flats_path
     else
       render :new, status: :unprocessable_entity
     end
-
   end
 
   def show
 
   end
 
+  def edit
+  end
+
+  def update
+    @flat.user = current_user
+    if @flat.update(flat_params)
+      redirect_to @flats_path, notice: 'Upadated successfully!'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :description, :price)
+    params.require(:flat).permit(:name, :description, :price, :address)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 end
