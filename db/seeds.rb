59b1eccc8a7cb8135403d6fceb80f8fee3ca0e0d
Agne Steponaviciute <agne.step@outlook.com>
2023-08-25 19:59:54 +0100

    flat index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+require 'literate_randomizer'
+
+user = User.create!(email: "agnea@gmail.com", password: "hellohello")
+
+20.times do
+  flat = Flat.new(
+    name: Faker::Name.name,
+    description: LiterateRandomizer.paragraph,
+    price: (50..1000).to_a.sample,
+    address: Faker::Address.full_address,
+    user_id: user.id
+  )
+  flat.save!
+end
