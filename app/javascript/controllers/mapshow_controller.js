3d7284de4dc9ce2d774528a76fedd888dba92032
amirn003 <amir.naar@gmail.com>
2023-08-31 23:43:05 +0200

    fix marker to map

--- app/javascript/controllers/mapshow_controller.js
+++ app/javascript/controllers/mapshow_controller.js
@@ -25,19 +25,15 @@ export default class extends Controller {
   }
 
   #addMarkersToMap() {
-
+    this.markerValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-
-      // Create a HTML element for your custom marker
       const customMarker = document.createElement("div")
       customMarker.innerHTML = marker.marker_html
-
-      // Pass the element as an argument to the new marker
       new mapboxgl.Marker(customMarker)
         .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
         .addTo(this.mapshow)
-
+    })
   }
 
   #fitMapToMarkers() {
