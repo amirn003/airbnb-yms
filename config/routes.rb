7e208d2dd49e257d2956adceca5e0be5fcfc9bb2
amirn003 <amir.naar@gmail.com>
2023-09-02 00:07:33 +0200

    create booking mngt, add link to bookings and edit profile to the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'booking', to: 'pages#booking'
 
   resources :flats do
     resources :bookings, only: [:create, :new]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+
 
   # Defines the root path route ("/")
   # root "articles#index"
