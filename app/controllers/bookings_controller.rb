88eea9fa59939244eac1f07f5c9a802fe309918a
amirn003 <amir.naar@gmail.com>
2023-09-04 23:26:22 +0200

    polish navbar, fix conditional view in show, redirect to booking page when submiting a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,7 +14,7 @@ class BookingsController < ApplicationController
     authorize @flat
 
     if @booking.save!
-      redirect_to flat_path(@flat)
+      redirect_to bookings_path
     else
       render :new, status: :unprocessable_entity
     end
